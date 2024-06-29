e78b5a0334a1a65573cb1e097697a864e2f27732
Prajwal <prajwalprasad7.ppmf@gmail.com>
2024-06-29 16:56:48 +0530

    Initial Commit

---
+++ PrajwalCard/PrajwalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrajwalCardApp.swift
+//  PrajwalCard
+//
+//  Created by Prajwal Prasad on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PrajwalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
